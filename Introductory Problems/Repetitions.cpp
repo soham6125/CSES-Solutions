b28a87da65d927fa8dc8b71749fa9e37896ec66d
soham6125 <70870489+soham6125@users.noreply.github.com>
2021-03-14 19:42:02 +0530

    Create Repetitions.cpp

---
+++ Introductory Problems/Repetitions.cpp
@@ -0,0 +1,56 @@
+#include<bits/stdc++.h>
+#include<vector>
+#include<iostream>
+#include<set>
+#include<string>
+#define ll long long int 
+#define loop(a,b,i) for(int i=a;i<b;i++)
+#define loopr(a,b,i) for(int i=n-1;i>=0;i--)
+#define pb push_back
+#define db double
+#define mp make_pair
+#define endl "\n"
+#define f first
+#define se second
+#define all(x) x.begin(),x.end()
+#define MOD 1000000007
+#define quick ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+ll add(ll x, ll y) {ll res = x + y; return (res >= MOD ? res - MOD : res);}
+ll mul(ll x, ll y) {ll res = x * y; return (res >= MOD ? res % MOD : res);}
+ll sub(ll x, ll y) {ll res = x - y; return (res < 0 ? res + MOD : res);}
+ll power(ll x, ll y) {ll res = 1; x %= MOD; while (y) {if (y & 1)res = mul(res, x); y >>= 1; x = mul(x, x);} return res;}
+ll mod_inv(ll x) {return power(x, MOD - 2);}
+using namespace std;
+ 
+int main()
+{ quick;
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+ 
+    // ll t;
+    // cin>>t;
+    // while(t--)
+    // {
+    	string s;
+    	cin>>s;
+    	ll n=s.length();
+    	ll cnt=1,ans=0;
+    	for(ll i=1;i<n;i++)
+    	{
+    		if(s[i]==s[i-1])
+    		{
+    			cnt++;
+    		}
+    		else
+    		{
+    			ans=max(ans,cnt);
+    			cnt=1;
+    		}
+    	}
+    	ans=max(ans,cnt);
+    	cout<<ans<<endl;
+    // }
+    return 0;
+}
