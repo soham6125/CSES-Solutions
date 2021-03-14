3784ecf4f6521486dcea040f370372224017bc28
soham6125 <70870489+soham6125@users.noreply.github.com>
2021-03-14 19:55:17 +0530

    Create Coin Piles.cpp

---
+++ Introductory Problems/Coin Piles.cpp
@@ -0,0 +1,52 @@
+#include<bits/stdc++.h>
+#include<vector>
+#include<iostream>
+#include<set>
+#include<string>
+#define ll long long int 
+#define loop(a,b,i) for(int i=a;i<b;i++)
+#define loopr(a,b,i) for(int i=n-1;i>=0;i--)
+#define pb push_back
+#define db double
+#define mp make_pair
+#define endl "\n"
+#define f first
+#define se second
+#define all(x) x.begin(),x.end()
+#define mod 1000000007
+#define quick ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+ll add(ll x, ll y) {ll res = x + y; return (res >= mod ? res - mod : res);}
+ll mul(ll x, ll y) {ll res = x * y; return (res >= mod ? res % mod : res);}
+ll sub(ll x, ll y) {ll res = x - y; return (res < 0 ? res + mod : res);}
+ll power(ll x, ll y) {ll res = 1; x %= mod; while (y) {if (y & 1)res = mul(res, x); y >>= 1; x = mul(x, x);} return res;}
+ll mod_inv(ll x) {return power(x, mod - 2);}
+using namespace std;
+ 
+int main()
+{ quick;
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+ 
+    ll t;
+    cin>>t;
+    while(t--)
+    {
+    	ll a,b;
+    	cin>>a>>b;
+    	if(a*2<b || b*2<a)
+    		cout<<"NO"<<endl;
+    	else
+    	{
+    		a=a%3;b=b%3;
+    		if(b==0 && a==0)
+    			cout<<"YES"<<endl;
+    		else if( (a==1 && b==2) || (a==2 && b==1) )
+    			cout<<"YES"<<endl;
+    		else
+    			cout<<"NO"<<endl;
+    	}
+    }
+    return 0;
+}
