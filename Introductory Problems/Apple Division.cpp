5a264f66a29810caa6c3ed518d9cc71d622f6c9d
soham6125 <70870489+soham6125@users.noreply.github.com>
2021-03-14 20:17:00 +0530

    Create Apple Division.cpp

---
+++ Introductory Problems/Apple Division.cpp
@@ -0,0 +1,62 @@
+#include<bits/stdc++.h>
+#include<vector>
+#include<iostream>
+#include<set>
+#include<string>
+#define ll long long int 
+#define loop(a,b,i) for(int i=a;i<b;i++)
+#define loopr(a,b,i) for(int i=n-1;i>=0;i--)
+#define pb push_back
+#define db double
+#define mp make_pair
+#define endl "\n"
+#define f first
+#define se second
+#define all(x) x.begin(),x.end()
+#define mod 1000000007
+#define quick ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+ll add(ll x, ll y) {ll res = x + y; return (res >= mod ? res - mod : res);}
+ll mul(ll x, ll y) {ll res = x * y; return (res >= mod ? res % mod : res);}
+ll sub(ll x, ll y) {ll res = x - y; return (res < 0 ? res + mod : res);}
+ll power(ll x, ll y) {ll res = 1; x %= mod; while (y) {if (y & 1)res = mul(res, x); y >>= 1; x = mul(x, x);} return res;}
+ll mod_inv(ll x) {return power(x, mod - 2);}
+using namespace std;
+ 
+ 
+int main()
+{ quick;
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+ 
+    ll t=1;
+    // cin>>t;
+    while(t--)
+    {
+    	ll n;
+    	cin>>n;
+    	ll a[n];
+    	ll sum=0;
+    	loop(0,n,i)
+    	{
+    		cin>>a[i];
+    		sum+=a[i];
+    	}
+    	ll ans=INT_MAX;
+    	for(ll i=0;i<(1<<n);i++)
+    	{
+    		ll temp=0;
+    		for(ll j=0;j<n;j++)
+    		{
+    			if(i & (1<<j))
+    				temp+=a[j];
+    		}
+    		ll x=abs(sum-(2*temp));
+    		ans=min(ans,x);
+    	}
+    	cout<<ans<<endl;
+    }
+ 
+    return 0;
+}
