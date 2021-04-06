33b1d5384673f586385b55bfa6d73eab85620aa0
Soham Chaudhuri <70870489+soham6125@users.noreply.github.com>
2021-04-06 12:26:33 +0530

    Create Subarray Sums I.cpp

---
+++ Sorting and Searching/Subarray Sums I.cpp
@@ -0,0 +1,61 @@
+#include<bits/stdc++.h>
+#include<vector>
+#include<iostream>
+#include<string>
+#include<unordered_map>
+#define ll long long int 
+#define loop(a,b,i) for(long long int i=a;i<b;i++)
+#define loopr(a,b,i) for(long long int i=a-1;i>=b;i--)
+#define pb push_back
+#define db double
+#define mp make_pair
+#define endl "\n"
+#define f first
+#define se second
+#define vll vector<ll>
+#define pll pair<ll,ll>
+#define vpll vector< pair<ll,ll> >
+#define all(x) x.begin(),x.end()
+#define quick ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+ 
+const int mod = 1000000007;
+ll add(ll x, ll y) {ll res = x + y; return (res >= mod ? res - mod : res);}
+ll mul(ll x, ll y) {ll res = x * y; return (res >= mod ? res % mod : res);}
+ll sub(ll x, ll y) {ll res = x - y; return (res < 0 ? res + mod : res);}
+ll power(ll x, ll y) {ll res = 1; x %= mod; while (y) {if (y & 1)res = mul(res, x); y >>= 1; x = mul(x, x);} return res;}
+ll mod_inv(ll x) {return power(x, mod - 2);}
+using namespace std;
+ 
+int main() 
+{ quick;  
+#ifndef ONLINE_JUDGE
+  freopen("input.txt", "r", stdin);
+  freopen("output.txt", "w", stdout);
+#endif
+ 
+    int tc = 1;
+    // cin>>tc;
+    while(tc--)
+    { 
+      ll n,sum;
+      cin>>n>>sum;
+      ll a[n];
+      ll ans = 0;
+      loop(0,n,i) cin>>a[i];
+      unordered_map<ll, ll> prevSum; 
+      ll currsum = 0; 
+      for(ll i = 0; i < n; i++) 
+      { 
+        currsum += a[i]; 
+        if(currsum == sum) 
+            ans++; 
+        if (prevSum.find(currsum - sum) != prevSum.end()) 
+            ans += (prevSum[currsum - sum]); 
+        prevSum[currsum]++; 
+      } 
+      cout<<ans;
+    }
+ 
+cerr << "time taken : " << (float)clock() / CLOCKS_PER_SEC << " secs" << endl;
+return 0;
+}
